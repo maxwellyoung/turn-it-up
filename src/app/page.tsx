6685771efdbfb19f5ea20ab6fde2633fb7c0858d
Maxwell Young <maxtheyoung@gmail.com>
2025-03-03 15:09:37 +1300

    video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -164,7 +164,7 @@ export default function Home() {
               controls
               autoPlay
               loop
-              src="/turnitup.mov"
+              src="/turnitup.mp4"
               style={{ colorScheme: "dark" }}
             />
           </div>
